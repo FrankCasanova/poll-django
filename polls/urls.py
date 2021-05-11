1f0140b6b185fd62d8670ea6a5f0bbb39501a63c
Frank Casanova <frankcasanova.info@gmail.com>
2021-05-11 12:56:26 +0200

    añadidos templates, vistas y urls

--- polls/urls.py
+++ polls/urls.py
@@ -6,6 +6,12 @@ from . import views
 #each connection going us to a view functionality
 #these connections needs to be connect with url root, because that's where the requests come from
 
+
+app_name = 'polls'
 urlpatterns = [
-    path('', views.index, name='index')
-]
+    path('', views.index, name='index'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
+    path('<int:question_id>/result/', views.result, name='result'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+] 
--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from polls.models import Question
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, response
+from django.template import loader
+from django.http import Http404
 
 # Create your views here.
 
+
 def index(request):
-    #first we create the funcionality, and then create the connection
-    #the connections are the url's,
-    # there live all connections that activate our functionalities  
-    return HttpResponse('Hello, world. You\'re at the polls index')
+    # first we create the funcionality, and then create the connection
+    # the connections are the url's,
+    # there live all connections that activate our functionalities
+    
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    # return HttpResponse(template.render(context,request))
+    return render(request, 'polls/index.html', context)
+    
+    
+    
+    
+
+
+def detail(request, question_id):
+    
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question does not exist')
+    question = get_object_or_404(Question, pk=question_id)    
+    
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def result(request, question_id):
+    response = f'your\'re looking at the resutls of question {question_id}'
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse(f'Your\'re voting on question {question_id}')
